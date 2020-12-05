c91a1dbd01dad1c35402f3e20d30fbfa9748004b
David Kane <david.kane@photobox.com>
2020-12-05 23:46:40 +0000

    Day 5 clean up

--- src/day5/index.ts
+++ src/day5/index.ts
@@ -1,26 +1,12 @@
-const findSeat = (directions: string[], [lower, upper]: number[]): number => {
-  const direction = directions.shift();
-  const change = ((upper - lower) + 1) / 2;
-  lower = direction === "B" || direction === "R" ? lower + change : lower;
-  upper = direction === "F" || direction === "L" ? upper - change : upper;
-  return directions.length ? findSeat(directions, [lower, upper]) : lower;
-};
-
 const getSeatId = (seat: string): number => {
-  const row = findSeat(seat.slice(0, 7).split(""), [0, 127]);
-  const column = findSeat(seat.slice(7).split(""), [0, 7]);
-  return row * 8 + column;
+  return parseInt(seat.replace(/[FL]/g, "0").replace(/[RB]/g, "1"), 2);
 };
 
 export const part1 = (input: string[]): number => {
-  return input.reduce((highest, seat) => {
-    const seatId = getSeatId(seat);
-    return seatId > highest ? seatId : highest;
-  }, 0);
+  return Math.max(...input.map(getSeatId));
 };
 
 export const part2 = (input: string[]): number => {
   const seats = input.map(getSeatId).sort((a, b) => a - b);
-  const previousSeat = seats.find((seat, i) => seat + 1 !== seats[i + 1]);
-  return (previousSeat ?? 0) + 1;
+  return (seats.find((seat, i) => seat + 1 !== seats[i + 1]) ?? 0) + 1;
 };
