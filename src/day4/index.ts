61ace0872311b942b23a2b4d0d80d03fc15b045b
David Kane <david.kane@photobox.com>
2020-12-05 10:08:44 +0000

    Day 4 clean up

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,15 +1,34 @@
+const required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+const hasRequiredFields = (passport: string): boolean => {
+  return required.every((field) => {
+    const regex = new RegExp(`${field}:`);
+    return passport.match(regex);
+  });
+};
+
 export const part1 = (input: string[]): number => {
-  return input.reduce((valid, passport) => {
-    const fields = passport.match(/(byr)|(iyr)|(eyr)|(hgt)|(hcl)|(ecl)|(pid)/g);
-    return fields && fields.length === 7 ? valid + 1 : valid;
+  return input.reduce((count, passport) => {
+    return hasRequiredFields(passport) ? count + 1 : count;
   }, 0);
 };
 
 export const part2 = (input: string[]): number => {
-  return input.reduce((valid, passport) => {
-    const fields = passport.match(
-      /(byr:19[2-9]\d|byr:200[0-2])|(iyr:201\d|iyr:2020)|(eyr:202\d|eyr:2030)|(hgt:1[5-8]\dcm|hgt:19[0-3]cm)|(hgt:59in|hgt:6[0-9]in|hgt:7[0-6]in)|(hcl:#[0-9a-f]{6})|(ecl:amb|ecl:blu|ecl:brn|ecl:gry|ecl:grn|ecl:hzl|ecl:oth)|(pid:\d{9})\s|(pid:\d{9})$/g,
-    );
-    return fields && fields.length === 7 ? valid + 1 : valid;
+  return input.reduce((count, passport) => {
+    if (!hasRequiredFields(passport)) {
+      return count;
+    }
+
+    const hgtCm = passport.match(/(hgt:1[5-8]\dcm|hgt:19[0-3]cm)(\s|$)/);
+    const hgtIn = passport.match(/(hgt:59in|hgt:6[0-9]in|hgt:7[0-6]in)(\s|$)/);
+    const ecl = passport.match(/(ecl:)(amb|blu|brn|gry|grn|hzl|oth)(\s|$)/);
+
+    if ((!hgtCm && !hgtIn) || !ecl) return count;
+    if (!passport.match(/(byr:19[2-9]\d|byr:200[0-2])(\s|$)/)) return count;
+    if (!passport.match(/(iyr:201\d|iyr:2020)(\s|$)/)) return count;
+    if (!passport.match(/(eyr:202\d|eyr:2030)(\s|$)/)) return count;
+    if (!passport.match(/(hcl:#[0-9a-f]{6})(\s|$)/)) return count;
+    if (!passport.match(/(pid:\d{9})(\s|$)/)) return count;
+
+    return count + 1;
   }, 0);
 };
