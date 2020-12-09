4a1d2ee88c0f0e689979a5e5e30ba3525e2b7ef5
David Kane <david.kane@photobox.com>
2020-12-09 11:16:51 +0000

    Clean up

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,17 +1,3 @@
-const getContiguousSet = (input: number[], target: number) => {
-  for (let i = 0; i < input.length; i++) {
-    const set = [input[i]];
-
-    for (let j = i + 1; j < input.length; j++) {
-      set.push(input[j]);
-
-      if (set.reduce((acc, val) => acc + val, 0) === target) {
-        return set.sort((a, b) => a - b);
-      }
-    }
-  }
-};
-
 export const part1 = (input: number[], length: number): number | undefined => {
   return input.slice(length).find((value, index) => {
     const preamble = input.slice(index, index + length);
@@ -21,6 +7,15 @@ export const part1 = (input: number[], length: number): number | undefined => {
 
 export const part2 = (input: number[], length: number): number | undefined => {
   const invalidNumber = part1(input, length) ?? 0;
-  const set = getContiguousSet(input, invalidNumber);
-  return set ? set[0] + set[set.length - 1] : 0;
+
+  for (let i = 0; i < input.length; i++) {
+    let sum = input[i];
+    const set = [input[i]];
+
+    for (let j = i + 1; j < input.length; j++) {
+      sum += input[j];
+      set.push(input[j]);
+      if (sum === invalidNumber) return Math.min(...set) + Math.max(...set);
+    }
+  }
 };
